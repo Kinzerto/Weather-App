905d956951aa2508bc7a8f87590af940833e9ae7
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-05-14 15:29:15 +0800

    refactor: changed error handing info

--- src/api/astronomyAPI.js
+++ src/api/astronomyAPI.js
@@ -4,7 +4,7 @@ export async function astronomy(lat, lon) {
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error('Failed to fetch astronomy');
+      throw new Error(`HTTP Error: ${response.status}`);
     }
     const data = await response.json();
     return data;
