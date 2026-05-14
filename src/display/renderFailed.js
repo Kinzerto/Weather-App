f6522a3d778c58a6fef7aa7213ef51278eaa0af2
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-05-14 21:00:30 +0800

    refactor: hide city display on fetch failure

--- src/display/renderFailed.js
+++ src/display/renderFailed.js
@@ -1,6 +1,10 @@
 import faildImg from '../assets/images/f-mark.svg';
+
 const content = document.querySelector('.content');
+const cityLoc = document.querySelector('.city');
+
 export function failed() {
+  cityLoc.textContent = '';
   const main = `
 <div class="failedContainer">
   <img src="${faildImg}" alt="" />
